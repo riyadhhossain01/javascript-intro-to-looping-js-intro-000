9e2a0efb042ec17a5155b2b04ab542ad97f354f7
riyadh hossain <riyadh_h@ymail.com>
2018-09-08 17:46:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 
 
 function forLoop(array){
+<<<<<<< HEAD
   for(let i = 0; i<25; i++){
+=======
+  for(int i = 0; i<25; i++){
+>>>>>>> cd745066c00f9b355f4c2154e263f34a684310d6
     if(i==1){
       array.push("I am 1 strange loop.")
     }else{
@@ -12,6 +16,7 @@ function forLoop(array){
 }
 
 function whileLoop(n){
+<<<<<<< HEAD
 var string = 'done'
   while(n>0){
     console.log(n);
@@ -30,4 +35,20 @@ while(array.length > 0 && maybeTrue()){
    array.length--;
  }
  return array;
+=======
+int count = 0;
+  while(n<0){
+    count++;
+    n--;
+  }
+  return count;
+}
+
+
+function doWhileLoop (array){
+  while(array.length > 0 && maybeTrue){
+    array.pop()
+  }
+  return array;
+>>>>>>> cd745066c00f9b355f4c2154e263f34a684310d6
 }
